9f8a24f5798e2512abe4a0f7067f35247dbd16bf
Cedric Boucher <cedricjboucher@pm.me>
2024-10-31 13:08:36 -0600

    finish User and major improvements and fixes

--- src/app/src/main/java/com/example/myapplication/User.java
+++ src/app/src/main/java/com/example/myapplication/User.java
@@ -1,86 +1,143 @@
 package com.example.myapplication;
 
+import android.graphics.Bitmap;
+
 public class User {
     private String name;
     private String email;
     private Integer phoneNumber;
-    private String profilePicture;
+    private Bitmap profilePicture;
     private boolean isAdmin;
-    private boolean isOrganizer;
     private Facility facility;
-    private boolean notification;
+    private boolean receivesOrgAdmNotifications;
 
-    public User(String name, String email, Integer phoneNumber, String profilePicture, boolean isAdmin, boolean isOrganizer, Facility facility){
-        // FIXME use setters here when possible to make use of validation code
-        this.name=name;
-        this.email=email;
-        this.phoneNumber=phoneNumber;
-        this.profilePicture=profilePicture; // FIXME profile picture may not be provided by user and must then be auto-generated
-        this.isAdmin=isAdmin;
-        this.isOrganizer=isOrganizer;
-        this.facility=facility;
-        //firebase db
+    // TODO constructor that gets info from database
+
+    public User(String name, String email) throws Exception {
+        this.setName(name); // it is important that name is set before profile picture
+        this.setEmail(email);
+        this.setPhoneNumber(null);
+        this.setProfilePicture(null); // FIXME profile picture may not be provided by user and must then be auto-generated
+        this.isAdmin = false; // TODO check DB
+        this.setFacility(null);
+        this.setReceivesOrgAdmNotifications(true);
     }
 
-    public void setName(String name){
-        // TODO validate name and throw exception otherwise
-        this.name=name;
+    public User(String name, String email, Integer phoneNumber) throws Exception {
+        this(name, email);
+        this.setPhoneNumber(phoneNumber);
     }
 
-    public void setEmail(String email){
-        // TODO validate email and throw exception otherwise
-        this.email=email;
+    public User(String name, String email, Bitmap profilePicture) throws Exception {
+        this(name, email);
+        this.setProfilePicture(profilePicture);
     }
 
-    public void setPhoneNumber(Integer phoneNumber){
-        // TODO validate phone number and throw exception otherwise
-        this.phoneNumber=phoneNumber;
+    public User(String name, String email, Integer phoneNumber, Bitmap profilePicture) throws Exception {
+        this(name, email, phoneNumber);
+        this.setProfilePicture(profilePicture);
     }
 
-    public void setProfilePicture(String profilePicture){
-        this.profilePicture=profilePicture;
+    public void setName(String name) throws Exception {
+        if (name == null) {
+            throw new Exception("cannot set name to null");
+        }
+        if (name.isEmpty()) {
+            throw new Exception("name cannot be empty");
+        }
+        this.name = name;
+        // TODO update database
     }
 
-    public void setNotification(boolean notification){
-        this.notification=notification;
+    public void setEmail(String email) throws Exception {
+        if (email == null) {
+            throw new Exception("cannot set email to null");
+        }
+        if (email.isEmpty()) {
+            throw new Exception("email cannot be empty");
+        }
+        if (!email.contains("@") || !email.contains(".")) {
+            throw new Exception("invalid email");
+        }
+        this.email = email;
+        // TODO update database
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) throws Exception {
+        if (phoneNumber != null) {
+            if (phoneNumber <= 0) {
+                throw new Exception("phone number cannot be a negative number");
+            }
+            if (phoneNumber.toString().length() > 15) {
+                throw new Exception("invalid phone number, too many digits");
+            }
+            if (phoneNumber.toString().length() < 7) {
+                throw new Exception("invalid phone number, not enough digits");
+            }
+        }
+        // phone number of null is ok since it is optional,
+        // null indicates the user has not defined their phone number
+        this.phoneNumber = phoneNumber;
+        // TODO update database
+    }
+
+    public void setProfilePicture(Bitmap profilePicture) {
+        if (profilePicture == null) {
+            // this is ok,
+            // but it means we have to auto-generate a profile picture based on user's name
+            // TODO generate profile picture based on user's name
+        }
+        this.profilePicture = profilePicture;
+        // TODO update database
+    }
+
+    public void deleteProfilePicture() {
+        this.setProfilePicture(null); // deletes and generates from user's name
+    }
+
+    public void setFacility(Facility facility) {
+        this.facility = facility;
+        // TODO update database
+    }
+
+    public void setReceivesOrgAdmNotifications(boolean receivesOrgAdmNotifications){
+        this.receivesOrgAdmNotifications = receivesOrgAdmNotifications;
+        // TODO update database
+    }
+
+    public void deleteFacility() {
+        this.setFacility(null);
     }
 
     public String getName(){
-        return name;
+        return this.name;
     }
 
     public String getEmail(){
-        return email;
+        return this.email;
     }
 
     public Integer getPhoneNumber(){
-        return phoneNumber;
+        return this.phoneNumber;
     }
 
-    public String getProfilePicture(){
-        return profilePicture;
+    public Bitmap getProfilePicture(){
+        return this.profilePicture;
+    }
+
+    public Facility getFacility(){
+        return this.facility;
     }
 
     public boolean isAdmin(){
-        return isAdmin;
+        return this.isAdmin;
     }
 
     public boolean isOrganizer(){
-        return isOrganizer;
+        return (this.facility != null); // user is an organizer if they have a facility
     }
 
-    public Facility facility(){
-        return facility;
+    public boolean getReceivesOrgAdmNotifications() {
+        return this.receivesOrgAdmNotifications;
     }
-
-    public boolean getNotification() {
-        return notification;
-    }
-
-    public void deleteFacility(String facilityID){
-
-    }
-
-
-
 }
