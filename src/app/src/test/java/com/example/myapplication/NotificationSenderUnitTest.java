85fed629ea061535337c69b08a26abdcafbf96d9
Cedric Boucher <cedricjboucher@pm.me>
2024-10-31 16:00:38 -0600

    add basic partial test case for NotificationSenderUnitTest

--- src/app/src/test/java/com/example/myapplication/NotificationSenderUnitTest.java
+++ src/app/src/test/java/com/example/myapplication/NotificationSenderUnitTest.java
@@ -1,12 +1,20 @@
 package com.example.myapplication;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class NotificationSenderUnitTest {
     @Test
-    public void sampleTest() {
-        assertEquals(4, 2 + 2);
+    public void sendNotificationTest() {
+        User user = null;
+        try {
+            user = new User("name", "email@test.ca");
+        }
+        catch (Exception exception) {
+            fail();
+        }
+        NotificationSender.sendNotification(user, "notification");
+        // TODO test that notification is actually received by the user
     }
 }
