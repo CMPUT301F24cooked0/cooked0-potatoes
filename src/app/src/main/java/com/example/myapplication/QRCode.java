59783a48f01c02f5c39bae7aa36acc19994a05b3
Cedric Boucher <onebitalpha@gmail.com>
2024-10-30 22:02:03 -0600

    implement all QRCode methods except getImage

--- src/app/src/main/java/com/example/myapplication/QRCode.java
+++ src/app/src/main/java/com/example/myapplication/QRCode.java
@@ -1,4 +1,31 @@
 package com.example.myapplication;
 
+import android.media.Image;
+
 public class QRCode {
+    private String text;
+
+    public QRCode(String text) {
+        this.text = text;
+    }
+
+    public QRCode() { // if you want to set the text later
+        this.text = null;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText(String text) {
+        return this.text;
+    }
+
+    public Image getImage() {
+        if (this.text == null) {
+            return null;
+        }
+        // TODO implement QR code generation
+        return null; // FIXME temporary since there is no implementation yet
+    }
 }
