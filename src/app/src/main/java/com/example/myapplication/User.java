d92aab95d9c0bfe3bd75e42ed5e01ec9be0ad23d
Ociliv <94505855+Ociliv1@users.noreply.github.com>
2024-10-30 21:45:10 -0600

    added setter and getters to user

--- src/app/src/main/java/com/example/myapplication/User.java
+++ src/app/src/main/java/com/example/myapplication/User.java
@@ -1,23 +1,82 @@
 package com.example.myapplication;
 
-import android.content.Context;
-
 public class User {
     private String name;
     private String email;
     private int phoneNumber;
+    private String profilePicture;
     private boolean isAdmin;
+    private boolean isOrganizer;
     private Facility facility;
     private boolean notification;
-    private EntrantStatus entrantStatus;
 
-    public User(String name, String email, int phoneNumber, boolean isAdmin, Facility facility, EntrantStatus entrantStatus){
+    public User(String name, String email, int phoneNumber, String profilePicture, boolean isAdmin, boolean isOrganizer, Facility facility){
         this.name=name;
         this.email=email;
         this.phoneNumber=phoneNumber;
+        this.profilePicture=profilePicture;
         this.isAdmin=isAdmin;
+        this.isOrganizer=isOrganizer;
         this.facility=facility;
-        this.entrantStatus=entrantStatus;
         //firebase db
     }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+
+    public void setProfilePicture(String profilePicture){
+        this.profilePicture=profilePicture;
+    }
+
+    public void setNotification(boolean notification){
+        this.notification=notification;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public String getProfilePicture(){
+        return profilePicture;
+    }
+
+    public boolean isAdmin(){
+        return isAdmin;
+    }
+
+    public boolean isOrganizer(){
+        return isOrganizer;
+    }
+
+    public Facility facility(){
+        return facility;
+    }
+
+    public boolean getNotification() {
+        return notification;
+    }
+
+    public void deleteFacility(String facilityID){
+
+    }
+
+
+
 }
