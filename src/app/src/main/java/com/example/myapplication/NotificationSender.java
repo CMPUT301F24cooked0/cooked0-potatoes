d96503aa1be1fc8c7c71e6af43167f21fd9c0db8
Cedric Boucher <onebitalpha@gmail.com>
2024-10-30 21:10:17 -0600

    add constructor and method to NotificationSender, but no implementation

--- src/app/src/main/java/com/example/myapplication/NotificationSender.java
+++ src/app/src/main/java/com/example/myapplication/NotificationSender.java
@@ -1,4 +1,10 @@
 package com.example.myapplication;
 
-public class NotificationSender {
+final public class NotificationSender { // this is a static class
+    private NotificationSender() {
+        // private constructor to prevent instantiation
+    }
+    public static void sendNotification(User user, String notification) {
+        // TODO implement sending notifications by updating the database
+    }
 }
