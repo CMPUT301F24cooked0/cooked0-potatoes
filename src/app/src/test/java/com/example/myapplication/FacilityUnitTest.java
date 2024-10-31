75cf612b0924001ea7b74023ef2e96f969332d1a
Cedric Boucher <cedricjboucher@pm.me>
2024-10-31 13:34:53 -0600

    create unit test files for base classes

---
+++ src/app/src/test/java/com/example/myapplication/FacilityUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FacilityUnitTest {
+    @Test
+    public void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ src/app/src/test/java/com/example/myapplication/UserUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UserUnitTest {
+    @Test
+    public void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
