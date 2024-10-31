701cc9506f3d88514016a86e50dc5294387edf9a
Cedric Boucher <onebitalpha@gmail.com>
2024-10-30 21:45:42 -0600

    implement all EntrantPool methods, except drawEntrants

--- src/app/src/main/java/com/example/myapplication/EntrantPool.java
+++ src/app/src/main/java/com/example/myapplication/EntrantPool.java
@@ -1,4 +1,87 @@
 package com.example.myapplication;
 
+import android.location.Location;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
 public class EntrantPool {
+    private ArrayList<EntrantStatus> entrants;
+
+    public EntrantPool() {
+        this.entrants = new ArrayList<EntrantStatus>();
+    }
+
+    /**
+     * finds the EntrantStatus in the entrants list that contains the User/Entrant.
+     * returns null if none was found
+     * @param entrant
+     * @return
+     */
+    @Nullable
+    private EntrantStatus findEntrant(User entrant) {
+        // find entrant status with matching entrant
+        for (EntrantStatus entrantStatus : this.entrants) {
+            if (entrantStatus.getEntrant() == entrant) {
+                return entrantStatus; // found, return this one
+            }
+        }
+        // we've now looped through the whole list and found no match
+        return null;
+    }
+
+    public void addEntrant(User entrant, Location joinedFrom) throws EntrantAlreadyInPool {
+        if (entrant == null) {
+            return;
+        }
+        if (findEntrant(entrant) != null) {
+            // entrant is already in this pool, they cannot be added again
+            throw new EntrantAlreadyInPool("entrant cannot be added to pool that they are already in");
+        }
+        // joinedFrom can be null though
+        EntrantStatus entrantStatus = new EntrantStatus(entrant, joinedFrom);
+        this.entrants.add(entrantStatus);
+    }
+
+    public void removeEntrant(User entrant) {
+        if (entrant == null) {
+            return; // nothing to do here
+        }
+        EntrantStatus entrantStatus = findEntrant(entrant);
+        if (entrantStatus == null) {
+            return; // no entrant to remove
+        }
+        this.entrants.remove(entrant);
+    }
+
+    public void setEntrantStatus(User entrant, Status status) {
+        if (entrant == null) {
+            return;
+        }
+        EntrantStatus entrantStatus = findEntrant(entrant);
+        if (entrantStatus == null) {
+            return; // no entrant to change status of
+        }
+        entrantStatus.setStatus(status);
+    }
+
+    public ArrayList<User> getEntrants() {
+        ArrayList<User> entrants = new ArrayList<User>();
+        // add entrants from each entrant status into list
+        for (EntrantStatus entrantStatus : this.entrants) {
+            entrants.add(entrantStatus.getEntrant());
+        }
+        return entrants;
+    }
+
+    public ArrayList<EntrantStatus> getEntrantStatuses() {
+        return this.entrants;
+    }
+
+    public ArrayList<User> drawEntrants(int howMany) {
+        // TODO implement this method
+        // don't forget to update their statuses when drawing!
+        return new ArrayList<User>(); // temporary
+    }
 }
