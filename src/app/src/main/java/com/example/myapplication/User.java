589c6b60d78286852c361f6ffd40e8288ae741a9
Ociliv <94505855+Ociliv1@users.noreply.github.com>
2024-10-30 21:12:29 -0600

    added constructor in user

--- src/app/src/main/java/com/example/myapplication/User.java
+++ src/app/src/main/java/com/example/myapplication/User.java
@@ -1,4 +1,23 @@
 package com.example.myapplication;
 
+import android.content.Context;
+
 public class User {
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private boolean isAdmin;
+    private Facility facility;
+    private boolean notification;
+    private EntrantStatus entrantStatus;
+
+    public User(String name, String email, int phoneNumber, boolean isAdmin, Facility facility, EntrantStatus entrantStatus){
+        this.name=name;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.isAdmin=isAdmin;
+        this.facility=facility;
+        this.entrantStatus=entrantStatus;
+        //firebase db
+    }
 }
