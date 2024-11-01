2746560849f7d28fa6a776a519a333922cd1033e
Cedric Boucher <cedricjboucher@pm.me>
2024-11-01 14:32:34 -0600

    add `throws` to addEntrant in Event

--- src/app/src/main/java/com/example/myapplication/Event.java
+++ src/app/src/main/java/com/example/myapplication/Event.java
@@ -94,7 +94,7 @@ public class Event {
         // TODO update database
     }
 
-    public void addEntrant(User entrant, LatLng joinedFrom) {
+    public void addEntrant(User entrant, LatLng joinedFrom) throws EntrantAlreadyInPool {
         this.entrantPool.addEntrant(entrant, joinedFrom); // entrantPool does validation for us
     }
 
