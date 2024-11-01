8fd670f81f75b06b78d7aac8a7465f5cd5d0c56d
Cedric Boucher <cedricjboucher@pm.me>
2024-10-31 18:03:20 -0600

    write most unit tests for QRCodeUnitTest

--- src/app/src/test/java/com/example/myapplication/QRCodeUnitTest.java
+++ src/app/src/test/java/com/example/myapplication/QRCodeUnitTest.java
@@ -1,12 +1,32 @@
 package com.example.myapplication;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
 public class QRCodeUnitTest {
     @Test
-    public void sampleTest() {
-        assertEquals(4, 2 + 2);
+    public void firstConstructorTest() {
+        QRCode qrCode = new QRCode("text");
+    }
+
+    @Test
+    public void secondConstructorTest() {
+        QRCode qrCode = new QRCode();
+    }
+
+    @Test
+    public void setGetTextTest() {
+        QRCode qrCode = new QRCode();
+        assertNull(qrCode.getText());
+        String text = "text";
+        qrCode.setText(text);
+        assertEquals(qrCode.getText(), text);
+    }
+
+    @Test
+    public void getImageTest() {
+        // TODO test once implementation is done
     }
 }
