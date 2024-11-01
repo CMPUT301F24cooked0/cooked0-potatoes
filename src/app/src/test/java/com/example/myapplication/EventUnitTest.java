9d6067e250271fdc8f62c8a166973b7533880caa
Cedric Boucher <cedricjboucher@pm.me>
2024-11-01 11:38:59 -0600

    add comment about EventMock in EventUnitTest

--- src/app/src/test/java/com/example/myapplication/EventUnitTest.java
+++ src/app/src/test/java/com/example/myapplication/EventUnitTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**
+ * Use EventMock instead of Event for testing, as it removes the
+ * eventPoster requirement. See note on EventMock class for more info
+ */
 public class EventUnitTest {
     @Test
     public void sampleTest() {
