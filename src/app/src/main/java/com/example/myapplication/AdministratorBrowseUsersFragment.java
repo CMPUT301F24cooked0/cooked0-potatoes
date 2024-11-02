0b968c9e5d1ff3262e828bcc862a0ff7cdd55a26
Ociliv <94505855+Ociliv1@users.noreply.github.com>
2024-11-01 18:43:51 -0600

    working on AdministratorBrowseUsersFragment and UserArrayAdapter to implement delete user function.

--- src/app/src/main/java/com/example/myapplication/AdministratorBrowseUsersFragment.java
+++ src/app/src/main/java/com/example/myapplication/AdministratorBrowseUsersFragment.java
@@ -5,39 +5,67 @@ import static java.security.AccessController.getContext;
 
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.EventListener;
 import java.util.List;
 
 public class AdministratorBrowseUsersFragment extends Fragment {
-    private ListView userListView;
-    private ArrayList<User> userDataList;
-    private ArrayAdapter<User> userArrayAdapter;
+    ListView userList;
+    ArrayList<User> userDataList;
+    ArrayAdapter<User> userArrayAdapter;
+
     //private FirebaseFirestore  TODO database
     //private CollectionReference TODO database reference to users
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view= inflater.inflate(R.layout.administrator_browse_users,container, false);
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         //TODO initialise database and user reference
+       /*
+        db=FirebaseFirestore.getInstance();
+        userRef=db.collection("users");
 
         userListView=view.findViewById(R.id.administrator_browse_users_recyclerview);
         userDataList=new ArrayList<>();
+        userListInit();
+        */
 
-        initialiseUserList();
+        userArrayAdapter=new UserArrayAdapter(this,userDataList);
+        userList.setAdapter(userArrayAdapter);
 
-        userArrayAdapter=new UserArrayAdapter(getContext(), userDataList);
-        userListView.setAdapter(userArrayAdapter);
+        userRef.addSnapshotListener(new EventListener<QuerySnapshot>(){
+           @Override
+           public void onEvent(@Nullable QuerySnapshot querySnapshots, @Nullable FirebaseFirestoreException error) {
+               if (error != null) {
+                   Log.e("Firestore", error.toString());
+                   return;
+               }
+               if (querySnapshots != null) {
+                   userDataList.clear();
+                   for (QueryDocumentSnapshot doc : querySnapshots) {
+                       String name = doc.getString("name");
+                       String email = doc.getString("email");
+                       Long phoneNumber = doc.getLong("phoneNumber");
+                       User user = new User(name, email, phoneNumber);
 
-
+                       userDataList.add(user);
+                   }
+                   userArrayAdapter.notifyDataSetChanged();
+               }
+           }
+        });
 
     }
 }
