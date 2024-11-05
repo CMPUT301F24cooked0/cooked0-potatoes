4e8c5ec5b821848f442aa8262f75d6a314caebdd
Ociliv <94505855+Ociliv1@users.noreply.github.com>
2024-11-04 21:48:05 -0700

    added the custom dialog to match with figma and updated the code to implement the custom dialog layout

--- src/app/src/main/java/com/example/myapplication/AdministratorBrowseUsersFragment.java
+++ src/app/src/main/java/com/example/myapplication/AdministratorBrowseUsersFragment.java
@@ -4,6 +4,7 @@ package com.example.myapplication;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.icu.text.Transliterator;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
@@ -12,7 +13,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -38,84 +41,64 @@ public class AdministratorBrowseUsersFragment extends Fragment {
     ArrayList<User> userDataList;
     ArrayAdapter<User> userArrayAdapter;
 
-    private FirebaseFirestore  db;
+    private FirebaseFirestore db;
     private CollectionReference userRef;
 
     @Nullable
     @Override
-    public void onCreateView(@NonNull LayoutInflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        View view=inflater.inflate(R.layout.administrator_browse_users,container,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.administrator_browse_users, container, false);
+        db = FirebaseFirestore.getInstance();
+        userRef = db.collection("Users");
 
-        db=FirebaseFirestore.getInstance();
-        userRef=db.collection("users");
-
-        userList=view.findViewById(R.id.administrator_browse_users_recyclerview);
-        userDataList=new ArrayList<>();
-        userListInit();
-
-
-        userArrayAdapter=new UserArrayAdapter(requireContext(),userDataList);
+        userList = view.findViewById(R.id.administrator_browse_users_recyclerview);
+        userDataList = new ArrayList<>();
+        userArrayAdapter = new UserArrayAdapter(requireContext(), userDataList);
         userList.setAdapter(userArrayAdapter);
 
-        userRef.addSnapshotListener(new EventListener<QuerySnapshot>(){
-           @Override
-           public void onEvent(@Nullable QuerySnapshot querySnapshots, @Nullable FirebaseFirestoreException error) {
-               if (error != null) {
-                   Log.e("Firestore", error.toString());
-                   return;
-               }
-               if (querySnapshots != null) {
-                   userDataList.clear();
-                   for (QueryDocumentSnapshot doc : querySnapshots) {
-                       String name = doc.getString("name");
-                       String email = doc.getString("email");
-                       Long phoneNumber = doc.getLong("phoneNumber");
-                       User user = new User(name, email, phoneNumber);
-
-                       userDataList.add(user);
-                   }
-                   userArrayAdapter.notifyDataSetChanged();
-               }
-           }
-        });
-
-        userList.setOnClickListener(new AdapterView.onItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView,View view, int position, long id){
-                showDeletePage(position);
+        userRef.addSnapshotListener((querySnapshots, error) -> {
+            userDataList.clear();
+            for (QueryDocumentSnapshot doc : querySnapshots) {
+                String name = doc.getString("name");
+                String email = doc.getString("email");
+                Long phoneNumber -doc.getLong("phoneNumber");
+                User user = new User(Name, email, phoneNumber);
+                userDataList.add(user);
             }
+            userArrayAdapter.notifyDataSetChanged();
         });
+
+        userList.setOnItemClickListener(((adapterView, view1, position, id) -> showDeletePage(position)));
         return view;
     }
 
-    private void showDeletePage(final int position){
-        AlertDialog.Builder builder=new AlertDialog.Builder(requireActivity());
-        builder.setTitle("Remove User");
-        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                User user=userDataList.get(position);
-                deleteUser(user,position);
-            }
+
+    private void showDeletePage(LayoutInflater inflater, final int position) {
+        View dialogView = inflater.inflate(R.layout.delete_user_dialog, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        builder.setView(dialogView);
+        TextView dialogTitle = dialogView.findViewById(R.id.dialog_title);
+        dialogTitle.setText("Remove User");
+        Button cancelButton = dialogView.findViewById(R.id.dialog_cancel_button);
+        Button removeButton = dialogView.findViewById(R.id.dialog_remove_button);
+        AlertDialog dialog = builder.create();
+
+        cancelButton.setOnClickListener(view -> dialog.dismiss());
+        removeButton.setOnClickListener(view -> {
+            User user = userDataList.get(position);
+            deleteUser(user, position);
+            dialog.dismiss();
         });
-        builder.setNegativeButton("Cancel",null);
-        builder.create().show();
+        dialog.show();
     }
 
 
-    private void deleteUser(final User user, final int position){
-        userRef.document(user.getName()).delete().addOnSuccessListener(new OnSuccessListener<Void>(){ //TODO change to delete based on an ID to avoid deleting duplicates
-            @Override
-            public void onSuccess(Void aVoid){
-                Log.d("Firestore","User Deleted");
-                userDataList.remove(position);
-                userArrayAdapter.notifyDataSetChanged();
-            }
-        }).addOnFailureListener(new OnFailureListener(){
-            @Override
-            public void onFailure(@NonNull Exception e){
-                Log.w("Firestore","Error Deleting User");
-            }
-        });
+    private void deleteUser(final User user, final int position) {
+        userRef.document(user.getName()).delete().addOnSuccessListener(aVoid -> {
+            Log.d("Firestore", "User Deleted");
+            userDataList.remove(position);
+            userArrayAdapter.notifyDataSetChanged();
+        }).addOnFailureListener(e -> Log.w("Firestore", "Error Removing User"));
     }
 }
+
