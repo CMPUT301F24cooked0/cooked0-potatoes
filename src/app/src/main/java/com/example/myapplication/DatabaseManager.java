fd97211e0ac3cc9b4a6c6d34816a2db0e3548900
Cedric Boucher <cedricjboucher@pm.me>
2024-11-07 14:22:32 -0700

    add createUser method to DatabaseManager

--- src/app/src/main/java/com/example/myapplication/DatabaseManager.java
+++ src/app/src/main/java/com/example/myapplication/DatabaseManager.java
@@ -1,5 +1,28 @@
 package com.example.myapplication;
 
-public class DatabaseManager {
-    // TODO move all the database shit into here I guess
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class DatabaseManager { // static class
+    private final FirebaseFirestore db;
+
+    public DatabaseManager() {
+        this.db = FirebaseFirestore.getInstance();
+    }
+
+    public void createUser(User user) {
+        String userID = user.getUniqueID();
+        HashMap<String, Object> userData = new HashMap<>();
+        userData.put("isAdmin", false);
+        userData.put("name", user.getName());
+        userData.put("email", user.getEmail());
+        userData.put("phoneNumber", user.getPhoneNumber());
+        userData.put("profilePicture", user.getProfilePicture());
+        DocumentReference userRef = this.db.collection("users").document(userID);
+        userRef.set(userData);
+        CollectionReference facilityCol = userRef.collection("facility");
+    }
 }
