4e3f2b49762f4dd0c8414c2fd5257b2d43c6c460
Cedric Boucher <cedricjboucher@pm.me>
2024-11-07 14:41:02 -0700

    create getUser method in DatabaseManager

--- src/app/src/main/java/com/example/myapplication/DatabaseManager.java
+++ src/app/src/main/java/com/example/myapplication/DatabaseManager.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
+import android.graphics.Bitmap;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -42,6 +46,68 @@ public class DatabaseManager { // static class
     }
 
     public User getUser(String userID) {
+        DocumentReference userRef = this.db.collection("users").document(userID);
+        CollectionReference facilityCol = userRef.collection("facility");
+        DocumentReference facilityRef = facilityCol.document(); // FIXME import facility from database if there is one?
+        final User[] user = new User[1];
 
+        userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                HashMap<String, Object> userData = (HashMap<String, Object>) documentSnapshot.getData();
+                if (userData == null) {
+                    throw new UserDoesNotExist("this user does not exist in the database");
+                }
+
+                // get user data from document
+
+                Object isAdminTemp = userData.get("isAdmin");
+                if (isAdminTemp == null) {
+                    throw new UserDoesNotExist("this user was missing the isAdmin field");
+                }
+                boolean isAdmin = (boolean) isAdminTemp;
+
+                Object nameTemp = userData.get("name");
+                if (nameTemp == null) {
+                    throw new UserDoesNotExist("this user was missing the name field");
+                }
+                String name = (String) nameTemp;
+
+                Object emailTemp = userData.get("email");
+                if (emailTemp == null) {
+                    throw new UserDoesNotExist("this user was missing the email field");
+                }
+                String email = (String) emailTemp;
+
+                Object phoneNumberTemp = userData.get("phoneNumber");
+                if (phoneNumberTemp == null) {
+                    throw new UserDoesNotExist("this user was missing the phoneNumber field");
+                }
+                Long phoneNumber = (Long) phoneNumberTemp;
+
+                Object profilePictureTemp = userData.get("profilePicture");
+                if (profilePictureTemp == null) {
+                    throw new UserDoesNotExist("this user was missing the profilePicture field");
+                }
+                Bitmap profilePicture = (Bitmap) profilePictureTemp;
+
+                Object notificationTemp = userData.get("receivesOrgAdmNotifications");
+                if (notificationTemp == null) {
+                    throw new UserDoesNotExist("this user was missing the receivesOrgAdmNotifications field");
+                }
+                boolean receivesOrgAdmNotifications = (boolean) notificationTemp;
+
+                try {
+                    user[0] = new User(name, email, phoneNumber, profilePicture);
+                    // FIXME pass isAdmin?
+                    // FIXME pass notification preference?
+                } catch (Exception e) {
+                    user[0] = null;
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        return user[0];
     }
 }
