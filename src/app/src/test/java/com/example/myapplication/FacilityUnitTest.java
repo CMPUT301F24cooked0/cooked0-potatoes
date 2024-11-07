6482bdcb93e755bceb6ad2da2afb76f3248ba51a
Cedric Boucher <cedricjboucher@pm.me>
2024-11-07 14:04:48 -0700

    Revert "fix FacilityUnitTest"
    This reverts commit d52a1d0e8e39be7519083d5003e135c52e4e8b47.

--- src/app/src/test/java/com/example/myapplication/FacilityUnitTest.java
+++ src/app/src/test/java/com/example/myapplication/FacilityUnitTest.java
@@ -27,8 +27,8 @@ public class FacilityUnitTest {
     public void addEventTest() throws Exception {
         User user = new User("name", "email@email.ca", db);
         LatLng location = new LatLng(69.420, 42.69);
-        Facility facility = new Facility("name", location, user, db);
-        Event event = new EventMock("name", new Date(),  null, facility, db);
+        Facility facility = new Facility("name", location, user);
+        Event event = new EventMock("name", new Date(), null);
         assertEquals(facility.getEvents().size(), 0);
         facility.addEvent(event);
         assertEquals(facility.getEvents().size(), 1);
@@ -39,8 +39,8 @@ public class FacilityUnitTest {
     public void addDuplicateEventTest() throws Exception {
         User user = new User("name", "email@email.ca", db);
         LatLng location = new LatLng(69.420, 42.69);
-        Facility facility = new Facility("name", location, user, db);
-        Event event = new EventMock("name", new Date(),  null, facility, db);
+        Facility facility = new Facility("name", location, user);
+        Event event = new EventMock("name", new Date(), null);
         facility.addEvent(event);
         assertThrows(EventAlreadyExistsAtFacility.class, () -> {facility.addEvent(event);});
     }
@@ -49,8 +49,8 @@ public class FacilityUnitTest {
     public void deleteEventNotInFacilityTest() throws Exception {
         User user = new User("name", "email@email.ca", db);
         LatLng location = new LatLng(69.420, 42.69);
-        Facility facility = new Facility("name", location, user, db);
-        Event event = new EventMock("name", new Date(),  null, facility, db);
+        Facility facility = new Facility("name", location, user);
+        Event event = new EventMock("name", new Date(), null);
         facility.deleteEvent(event);
         assertEquals(facility.getEvents().size(), 0);
     }
@@ -59,8 +59,8 @@ public class FacilityUnitTest {
     public void deleteEventInFacilityTest() throws Exception {
         User user = new User("name", "email@email.ca", db);
         LatLng location = new LatLng(69.420, 42.69);
-        Facility facility = new Facility("name", location, user, db);
-        Event event = new EventMock("name", new Date(),  null, facility, db);
+        Facility facility = new Facility("name", location, user);
+        Event event = new EventMock("name", new Date(), null);
         facility.addEvent(event);
         facility.deleteEvent(event);
         assertEquals(facility.getEvents().size(), 0);
@@ -70,8 +70,8 @@ public class FacilityUnitTest {
     public void deleteAllEventsTest() throws Exception {
         User user = new User("name", "email@email.ca", db);
         LatLng location = new LatLng(69.420, 42.69);
-        Facility facility = new Facility("name", location, user, db);
-        Event event = new EventMock("name", new Date(),  null, facility, db);
+        Facility facility = new Facility("name", location, user);
+        Event event = new EventMock("name", new Date(), null);
         facility.addEvent(event);
         facility.deleteAllEvents();
         assertEquals(facility.getEvents().size(), 0);
