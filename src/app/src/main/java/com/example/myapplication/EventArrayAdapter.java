4a2f1f934dc1529da67ed768814f8c80a5edb8c2
Auroni Bashar <auronibee@gmail.com>
2024-11-08 10:57:16 -0700

    add event array adapter

---
+++ src/app/src/main/java/com/example/myapplication/EventArrayAdapter.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class EventArrayAdapter extends ArrayAdapter<Event> {
+    public EventArrayAdapter(Context context, ArrayList<Event> events) {super(context, 0, events);}
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.events_array_content,
+                    parent, false);
+        } else {
+            view = convertView;
+        }
+        Event event = getItem(position);
+        TextView eventTitle = view.findViewById(R.id.eventNamePlaceholder);
+        // TODO implement event description
+        // TextView eventDesc = view.findViewById(R.id.eventDescPlaceholder);
+        ImageView eventPoster = view.findViewById(R.id.eventPosterPlaceholder);
+        eventTitle.setText(event.getName());
+        eventPoster.setImageBitmap(event.getEventPoster());
+        return view;
+
+
+    }
+
+}
---
+++ src/app/src/main/java/com/example/myapplication/FacilityViewEventsFragment.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class FacilityViewEventsFragment extends AppCompatActivity {
+    TextView facilityName;
+    TextView facilityLink;
+    ListView eventList;
+    EventArrayAdapter eventAdapter;
+}
