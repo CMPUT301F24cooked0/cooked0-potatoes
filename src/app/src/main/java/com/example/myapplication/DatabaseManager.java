6ca100583c2fbe4a13e6fd84d67507934e93f01e
Cedric Boucher <onebitalpha@gmail.com>
2024-11-10 16:47:10 -0700

    create enums for the field names of each collection in the database

--- src/app/src/main/java/com/example/myapplication/DatabaseManager.java
+++ src/app/src/main/java/com/example/myapplication/DatabaseManager.java
@@ -27,12 +27,12 @@ public class DatabaseManager { // static class
         }
         String userID = user.getUniqueID();
         HashMap<String, Object> userData = new HashMap<>();
-        userData.put("isAdmin", user.isAdmin());
-        userData.put("name", user.getName());
-        userData.put("email", user.getEmail());
-        userData.put("phoneNumber", user.getPhoneNumber());
-        userData.put("profilePicture", user.getProfilePicture());
-        userData.put("receivesOrgAdmNotifications", user.getReceivesOrgAdmNotifications());
+        userData.put(DatabaseUserFieldNames.isAdmin.name(), user.isAdmin());
+        userData.put(DatabaseUserFieldNames.name.name(), user.getName());
+        userData.put(DatabaseUserFieldNames.email.name(), user.getEmail());
+        userData.put(DatabaseUserFieldNames.phoneNumber.name(), user.getPhoneNumber());
+        userData.put(DatabaseUserFieldNames.profilePicture.name(), user.getProfilePicture());
+        userData.put(DatabaseUserFieldNames.receivesOrgAdmNotifications.name(), user.getReceivesOrgAdmNotifications());
         DocumentReference userRef = this.db.collection(DatabaseCollectionNames.users.name()).document(userID);
         userRef.set(userData);
         CollectionReference facilityCol = userRef.collection(DatabaseCollectionNames.facilities.name());
@@ -46,11 +46,11 @@ public class DatabaseManager { // static class
             return;
         }
         HashMap<String, Object> userData = new HashMap<>();
-        userData.put("name", user.getName());
-        userData.put("email", user.getEmail());
-        userData.put("phoneNumber", user.getPhoneNumber());
-        userData.put("profilePicture", user.getProfilePicture());
-        userData.put("receivesOrgAdmNotifications", user.getReceivesOrgAdmNotifications());
+        userData.put(DatabaseUserFieldNames.name.name(), user.getName());
+        userData.put(DatabaseUserFieldNames.email.name(), user.getEmail());
+        userData.put(DatabaseUserFieldNames.phoneNumber.name(), user.getPhoneNumber());
+        userData.put(DatabaseUserFieldNames.profilePicture.name(), user.getProfilePicture());
+        userData.put(DatabaseUserFieldNames.receivesOrgAdmNotifications.name(), user.getReceivesOrgAdmNotifications());
         DocumentReference userRef = user.getUserReference();
         userRef.update(userData);
         this.updateFacility(user.getFacility());
@@ -70,37 +70,37 @@ public class DatabaseManager { // static class
 
                 // get user data from document
 
-                Object isAdminTemp = userData.get("isAdmin");
+                Object isAdminTemp = userData.get(DatabaseUserFieldNames.isAdmin.name());
                 if (isAdminTemp == null) {
                     throw new UserDoesNotExist("this user was missing the isAdmin field");
                 }
                 boolean isAdmin = (boolean) isAdminTemp;
 
-                Object nameTemp = userData.get("name");
+                Object nameTemp = userData.get(DatabaseUserFieldNames.name.name());
                 if (nameTemp == null) {
                     throw new UserDoesNotExist("this user was missing the name field");
                 }
                 String name = (String) nameTemp;
 
-                Object emailTemp = userData.get("email");
+                Object emailTemp = userData.get(DatabaseUserFieldNames.email.name());
                 if (emailTemp == null) {
                     throw new UserDoesNotExist("this user was missing the email field");
                 }
                 String email = (String) emailTemp;
 
-                Object phoneNumberTemp = userData.get("phoneNumber");
+                Object phoneNumberTemp = userData.get(DatabaseUserFieldNames.phoneNumber.name());
                 if (phoneNumberTemp == null) {
                     throw new UserDoesNotExist("this user was missing the phoneNumber field");
                 }
                 Long phoneNumber = (Long) phoneNumberTemp;
 
-                Object profilePictureTemp = userData.get("profilePicture");
+                Object profilePictureTemp = userData.get(DatabaseUserFieldNames.profilePicture.name());
                 if (profilePictureTemp == null) {
                     throw new UserDoesNotExist("this user was missing the profilePicture field");
                 }
                 Bitmap profilePicture = (Bitmap) profilePictureTemp;
 
-                Object notificationTemp = userData.get("receivesOrgAdmNotifications");
+                Object notificationTemp = userData.get(DatabaseUserFieldNames.receivesOrgAdmNotifications.name());
                 if (notificationTemp == null) {
                     throw new UserDoesNotExist("this user was missing the receivesOrgAdmNotifications field");
                 }
@@ -127,8 +127,8 @@ public class DatabaseManager { // static class
         DocumentReference userRef = user.getUserReference();
         DocumentReference facilityRef = userRef.collection(DatabaseCollectionNames.facilities.name()).document();
         HashMap<String, Object> facilityData = new HashMap<>();
-        facilityData.put("name", facility.getName());
-        facilityData.put("location", facility.getLocation());
+        facilityData.put(DatabaseFacilityFieldNames.name.name(), facility.getName());
+        facilityData.put(DatabaseFacilityFieldNames.location.name(), facility.getLocation());
         facilityRef.set(facilityData);
         CollectionReference eventCol = facilityRef.collection(DatabaseCollectionNames.events.name());
 
@@ -140,8 +140,8 @@ public class DatabaseManager { // static class
             return;
         }
         HashMap<String, Object> facilityData = new HashMap<>();
-        facilityData.put("name", facility.getName());
-        facilityData.put("location", facility.getLocation());
+        facilityData.put(DatabaseFacilityFieldNames.name.name(), facility.getName());
+        facilityData.put(DatabaseFacilityFieldNames.location.name(), facility.getLocation());
         DocumentReference facilityRef = facility.getFacilityReference();
         facilityRef.update(facilityData);
         for (Event event : facility.getEvents()) {
@@ -170,13 +170,13 @@ public class DatabaseManager { // static class
 
                 // get facility data from document
 
-                Object nameTemp = facilityData.get("name");
+                Object nameTemp = facilityData.get(DatabaseFacilityFieldNames.name.name());
                 if (nameTemp == null) {
                     throw new FacilityDoesNotExist("this facility was missing the name field");
                 }
                 String name = (String) nameTemp;
 
-                Object locationTemp = facilityData.get("location");
+                Object locationTemp = facilityData.get(DatabaseFacilityFieldNames.location.name());
                 if (locationTemp == null) {
                     throw new FacilityDoesNotExist("this facility was missing the location field");
                 }
@@ -200,7 +200,21 @@ public class DatabaseManager { // static class
     }
 
     public DocumentReference createEvent(Facility facility, Event event) {
+        if (facility == null || event == null) {
+            return null;
+        }
+        DocumentReference facilityRef = facility.getFacilityReference();
+        DocumentReference eventRef = facilityRef.collection(DatabaseCollectionNames.events.name()).document();
+        HashMap<String, Object> eventData = new HashMap<>();
+        eventData.put(DatabaseEventFieldNames.name.name(), event.getName());
+        eventData.put(DatabaseEventFieldNames.date.name(), event.getDate());
+        eventData.put(DatabaseEventFieldNames.eventPoster.name(), event.getEventPoster());
+        eventData.put(DatabaseEventFieldNames.qrCode.name(), event.getQrCode());
+        eventData.put(DatabaseEventFieldNames.capacity.name(), event.getCapacity());
+        eventRef.set(eventData);
+        CollectionReference entrantPoolCol =  eventRef.collection(DatabaseCollectionNames.entrantPools.name());
 
+        return eventRef;
     }
 
     public void updateEvent(Event event) {
