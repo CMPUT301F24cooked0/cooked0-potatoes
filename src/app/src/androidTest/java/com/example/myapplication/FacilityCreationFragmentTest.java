d367a5025f31cf5cfb512a02d955683abe1bb9f9
Auroni Bashar <auronibee@gmail.com>
2024-11-12 20:31:33 -0700

    converting facility creation activity to fragment - implementing passing on objects

---
+++ src/app/src/androidTest/java/com/example/myapplication/FacilityCreationFragmentTest.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+
+public class FacilityCreationFragmentTest {
+    @Rule
+    public ActivityScenarioRule<FacilityCreationActivity> scenario = new ActivityScenarioRule<FacilityCreationActivity>(FacilityCreationActivity.class);
+
+}
--- src/app/src/main/java/com/example/myapplication/FacilityCreationFragment.java
+++ src/app/src/main/java/com/example/myapplication/FacilityCreationFragment.java
@@ -1,21 +1,18 @@
 package com.example.myapplication;
 
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+public class FacilityCreationFragment extends Fragment {
 
-import com.google.android.gms.maps.model.LatLng;
-
-import java.io.IOException;
-import java.util.List;
-
-public class FacilityCreationFragment extends AppCompatActivity {
+    View view;
     EditText facilityNameInput;
     EditText facilityAddressInput;
     Button createFacilityButton;
@@ -23,50 +20,16 @@ public class FacilityCreationFragment extends AppCompatActivity {
     DatabaseManager databaseManager;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.facility_creation);
-        facilityNameInput = findViewById(R.id.addFacilityName);
-        facilityAddressInput = findViewById(R.id.addFacilityAddress);
-        createFacilityButton = findViewById(R.id.createFacilityButton);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.facility_creation, container, false);
+        facilityNameInput = view.findViewById(R.id.addFacilityName);
+        facilityAddressInput = view.findViewById(R.id.addFacilityAddress);
+        createFacilityButton = view.findViewById(R.id.createFacilityButton);
         databaseManager = new DatabaseManager();
-        facilityOwner = (User) getIntent().getSerializableExtra("user");
-        createFacilityButton.setOnClickListener(view -> {
-            // get facility name and address from input fields
-            String facilityName = facilityNameInput.getText().toString();
-            String facilityAddressStr = facilityAddressInput.getText().toString();
-            if (facilityName.isEmpty() || facilityAddressStr.isEmpty()) {
-                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            // convert address to LatLng
-            LatLng facilityAddress = getAddress(facilityAddressStr);
-            if (facilityAddress == null) {
-                Toast.makeText(this, "Invalid address", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            Facility facility = new Facility(facilityName, facilityAddress);
-            facilityOwner.setFacility(facility);
-            // add facility to database and set facility document reference
-            facility.setFacilityReference(databaseManager.createFacility(facilityOwner, facility));
 
 
-        });
-    }
-    public LatLng getAddress(String address) {
-        // converts string address given by user to LatLng
-        Geocoder geocoder = new Geocoder(this);
-        try {
-            List<Address> addresses = geocoder.getFromLocationName(address, 1);
-            if (addresses != null && !addresses.isEmpty()) {
-                Address location = addresses.get(0);
-                return new LatLng(location.getLatitude(), location.getLongitude());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Error getting address", Toast.LENGTH_SHORT).show();
-            return null;
-        }
-        return null;
+        return view;
     }
 }
