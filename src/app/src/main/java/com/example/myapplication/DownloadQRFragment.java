c5ca1ee84f0aa6ab29a2930590a46be6e2a7da3b
Auroni Bashar <auronibee@gmail.com>
2024-11-21 16:19:25 -0700

    add continue and setting qr image functionality to download qr fragment

---
+++ src/app/src/main/java/com/example/myapplication/DownloadQRFragment.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.zxing.WriterException;
+
+/**
+ *
+ */
+public class DownloadQRFragment extends Fragment {
+    View view;
+    QRCode eventQRCode;
+    Button downloadBtn;
+    Button continueBtn;
+    ImageView qrCodeImage;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_qr_download, container, false);
+        return view;
+
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        downloadBtn = view.findViewById(R.id.downloadButton);
+        continueBtn = view.findViewById(R.id.continueButton);
+        qrCodeImage = view.findViewById(R.id.qrCodeImage);
+        // eventQRCode = (QRCode) getArguments().getSerializable("qrCode"); // TODO get qr code from bundle once navigation is completed
+//        try { // TODO set qr image once navigation complete
+//            qrCodeImage.setImageBitmap(eventQRCode.getImage());
+//        } catch (WriterException e) {
+//            throw new RuntimeException(e);
+//        }
+        downloadBtn.setOnClickListener(this::onClickDownload);
+        continueBtn.setOnClickListener(this::onClickContinue);
+
+    }
+    public void onClickDownload (View view) {
+        // TODO download QR code
+    }
+    public void onClickContinue (View view) {
+//        Fragment fragment = new FacilityViewEventsFragment();
+//        FragmentManager fragmentManager = getParentFragmentManager();
+//        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit(); // TODO continue to view events page when navigation complete
+    }
+
+}
