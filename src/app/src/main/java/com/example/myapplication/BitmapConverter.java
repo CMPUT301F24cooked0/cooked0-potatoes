4ef882543374c92279079fa05f32d4322ddf9717
Cedric Boucher <cedricjboucher@pm.me>
2024-11-21 17:38:12 -0700

    create a BitmapConverter class for converting between Bitmap and String

---
+++ src/app/src/main/java/com/example/myapplication/BitmapConverter.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.graphics.Bitmap;
+
+public class BitmapConverter {
+    public static Bitmap StringToBitmap(String imageString) {
+        return;
+    }
+
+    public static String BitmapToString(Bitmap imageBitmap) {
+        return;
+    }
+}
