f44ee42aab5c02dbf84109cc7a33e76c8b626716
Auroni Bashar <auronibee@gmail.com>
2024-11-23 23:30:48 -0700

    create qr scanner fragment

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -39,10 +39,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.qr.generator)
     implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.qr.generator)
 }
---
+++ src/app/src/main/java/com/example/myapplication/QRScannerFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kotlin.annotation.Target;
+
+/**
+ * This fragment allows the user to scan a QR code that links to an event.
+ */
+public class QRScannerFragment extends Fragment {
+    View view;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_qr_scanner, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
