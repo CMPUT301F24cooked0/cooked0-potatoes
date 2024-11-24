459b758363645a715a506d611af6eeed5081e678
Auroni Bashar <auronibee@gmail.com>
2024-11-24 01:07:26 -0700

    add base scanner functionality and TODOs

--- src/app/src/main/java/com/example/myapplication/QRScannerFragment.java
+++ src/app/src/main/java/com/example/myapplication/QRScannerFragment.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import kotlin.annotation.Target;
 
@@ -17,6 +23,7 @@ import kotlin.annotation.Target;
  */
 public class QRScannerFragment extends Fragment {
     View view;
+    Button scanBtn;
 
 
     @Override
@@ -30,5 +37,20 @@ public class QRScannerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        scanBtn = view.findViewById(R.id.scan_button);
+        scanBtn.setOnClickListener(this::onClickScan);
+
+    }
+    public void onClickScan(View view) {
+        ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+                result -> {
+                    if (result.getContents() == null) { // TODO handle other qr code texts (not in our database oath format
+                        Toast.makeText(requireContext(), "Unable to get event details", Toast.LENGTH_LONG).show();
+                    } else {
+                        // TODO get event using qr text and set up fragment transaction to show event details/signup
+                    }
+                });
+        barcodeLauncher.launch(new ScanOptions().setDesiredBarcodeFormats(ScanOptions.QR_CODE).setPrompt("Scan a QR code")); // launch QR scanner
+
     }
 }
