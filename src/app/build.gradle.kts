3ffece4117cf76411fc2ce977aa8bdd606c0e8c6
Auroni Bashar <auronibee@gmail.com>
2024-11-23 23:21:24 -0700

    add qr scanning library

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     testImplementation(libs.mockito.core)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.qr.generator)
 }
