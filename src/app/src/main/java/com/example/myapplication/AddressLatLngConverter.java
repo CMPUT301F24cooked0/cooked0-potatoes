1ac3ce0913b7ba3de420a60f30820ed74f21a274
Auroni Bashar <auronibee@gmail.com>
2024-11-25 19:56:23 -0700

    implement class logic

--- src/app/src/main/java/com/example/myapplication/AddressLatLngConverter.java
+++ src/app/src/main/java/com/example/myapplication/AddressLatLngConverter.java
@@ -1,6 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
 
 public class AddressLatLngConverter {
     /**
@@ -8,8 +15,15 @@ public class AddressLatLngConverter {
      * @param addressStr
      * @return
      */
-    public static LatLng AddressStrToLatLng(String addressStr) {
-        return null;
+    public static LatLng AddressStrToLatLng(String addressStr, Context context) throws IOException {
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> addresses = geocoder.getFromLocationName(addressStr, 1);
+        if (addresses != null && !addresses.isEmpty()) {
+            Address location = addresses.get(0);
+            return new LatLng(location.getLatitude(), location.getLongitude());
+        } else {
+            return null;
+        }
     }
 }
 
