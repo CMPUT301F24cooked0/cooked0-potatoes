910929395351695f812a754eaf977c413b770cdd
Auroni Bashar <auronibee@gmail.com>
2024-11-25 19:50:11 -0700

    create class with static method

---
+++ src/app/src/main/java/com/example/myapplication/AddressLatLngConverter.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class AddressLatLngConverter {
+    /**
+     * Converts an address String to a LatLng.
+     * @param addressStr
+     * @return
+     */
+    public static LatLng AddressStrToLatLng(String addressStr) {
+        return null;
+    }
+}
+
