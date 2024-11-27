338836a1315aae0694a63f4d7866bdd58473b39b
Cedric Boucher <cedricjboucher@pm.me>
2024-11-27 15:39:19 -0700

    delete Fragment import in Facility base class

--- src/app/src/main/java/com/example/myapplication/Facility.java
+++ src/app/src/main/java/com/example/myapplication/Facility.java
@@ -1,7 +1,5 @@
 package com.example.myapplication;
 
-import androidx.fragment.app.Fragment;
-
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.firestore.DocumentReference;
 
