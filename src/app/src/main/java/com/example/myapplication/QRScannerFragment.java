49d2f35cde255c896b1d16af1933de1a86f14ee9
Auroni Bashar <auronibee@gmail.com>
2024-11-27 18:36:57 -0700

    create single event listener and let scanner fragment implement it

--- src/app/src/main/java/com/example/myapplication/QRScannerFragment.java
+++ src/app/src/main/java/com/example/myapplication/QRScannerFragment.java
@@ -28,13 +28,14 @@ import java.util.HashMap;
 /**
  * This fragment allows the user to scan a QR code that links to an event.
  */
-public class QRScannerFragment extends Fragment {
+public class QRScannerFragment extends Fragment implements OnSingleEventFetchListener {
     View view;
     Button scanBtn;
     Event eventToView;
     FirebaseFirestore db;
     DocumentReference eventRef;
     HashMap<String, Object> eventData;
+    DatabaseManager databaseManager;
 
 
     @Override
@@ -50,6 +51,7 @@ public class QRScannerFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         scanBtn = view.findViewById(R.id.scan_button);
         scanBtn.setOnClickListener(this::onClickScan);
+        this.eventToView = null;
 
     }
     public void onClickScan(View view) {
@@ -58,8 +60,8 @@ public class QRScannerFragment extends Fragment {
                     if (result.getContents() == null) { // TODO handle other qr code texts (not in our database path format)
                         Toast.makeText(requireContext(), "QR code contents cannot be read", Toast.LENGTH_LONG).show();
                     } else {
-                        eventToView = loadEventDetails(result.getContents());
-                        if (eventToView == null) { // event not found in database or qr code is not correct
+                        new DatabaseManager().getSingleEvent(result.getContents(), this); // get event from database
+                        if (this.eventToView == null) { // event not found in database or qr code is not correct
                             Toast.makeText(requireContext(), "Event not found", Toast.LENGTH_LONG).show();
                         } else {
                             // navigate to view event details fragment to show event details
@@ -77,43 +79,49 @@ public class QRScannerFragment extends Fragment {
         barcodeLauncher.launch(new ScanOptions().setDesiredBarcodeFormats(ScanOptions.QR_CODE).setPrompt("Scan Event QR code")); // launch QR scanner
 
     }
-        public Event loadEventDetails(String eventPath) {
-            db = FirebaseFirestore.getInstance();
-            eventRef = db.document(eventPath);
-            eventRef.get().addOnSuccessListener(documentSnapshot -> {
-                // if the document exists, get the data otherwise set it to null
-                if (documentSnapshot.exists()) {
-                    eventData = (HashMap<String, Object>) documentSnapshot.getData();
-                } else {
-                    eventData = null;
-                }
-            });
-            if (eventData != null) {
-                // get event data from document
-                Object nameTemp = eventData.get(DatabaseEventFieldNames.name.name());
-                Object dateTemp = eventData.get(DatabaseEventFieldNames.instant.name());
-                Object eventPosterTemp = eventData.get(DatabaseEventFieldNames.eventPoster.name());
-                Object qrCodeTemp = eventData.get(DatabaseEventFieldNames.qrCode.name());
-                QRCode qrCode = (QRCode) qrCodeTemp; // QR Code converted to object to easily check for id later on if it is stored here
-                Object capacityTemp = eventData.get(DatabaseEventFieldNames.capacity.name());
-                // check if all required fields exist and qr code is correct
-                if (nameTemp == null || dateTemp == null || eventPosterTemp == null || qrCodeTemp == null) {
-                    return null;
-                } else {
-                    String eventName = (String) nameTemp;
-                    Instant eventDate = (Instant) dateTemp;
-                    String encodedEventPoster = (String) eventPosterTemp;
-                    Bitmap eventPoster = BitmapConverter.StringToBitmap(encodedEventPoster);
-                    Integer capacity = (Integer) capacityTemp;
-                    try {
-                        return new Event(eventName, eventDate, eventPoster, capacity); // create event object
-                    } catch (Exception e) {
-                        return null;
-                    }
+    @Override
+    public void onSingleEventFetch(Event event) {
+        this.eventToView = event;
+    }
 
-                }
+//    public Event loadEventDetails(String eventPath) {
+//        db = FirebaseFirestore.getInstance();
+//        eventRef = db.document(eventPath);
+//        eventRef.get().addOnSuccessListener(documentSnapshot -> {
+//            // if the document exists, get the data otherwise set it to null
+//            if (documentSnapshot.exists()) {
+//                    eventData = (HashMap<String, Object>) documentSnapshot.getData();
+//            } else {
+//                eventData = null;
+//            }
+//            });
+//            if (eventData != null) {
+//                // get event data from document
+//                Object nameTemp = eventData.get(DatabaseEventFieldNames.name.name());
+//                Object dateTemp = eventData.get(DatabaseEventFieldNames.instant.name());
+//                Object eventPosterTemp = eventData.get(DatabaseEventFieldNames.eventPoster.name());
+//                Object qrCodeTemp = eventData.get(DatabaseEventFieldNames.qrCode.name());
+//                QRCode qrCode = (QRCode) qrCodeTemp; // QR Code converted to object to easily check for id later on if it is stored here
+//                Object capacityTemp = eventData.get(DatabaseEventFieldNames.capacity.name());
+//                // check if all required fields exist and qr code is correct
+//                if (nameTemp == null || dateTemp == null || eventPosterTemp == null || qrCodeTemp == null) {
+//                    return null;
+//                } else {
+//                    String eventName = (String) nameTemp;
+//                    Instant eventDate = (Instant) dateTemp;
+//                    String encodedEventPoster = (String) eventPosterTemp;
+//                    Bitmap eventPoster = BitmapConverter.StringToBitmap(encodedEventPoster);
+//                    Integer capacity = (Integer) capacityTemp;
+//                    try {
+//                        return new Event(eventName, eventDate, eventPoster, capacity); // create event object
+//                    } catch (Exception e) {
+//                        return null;
+//                    }
+//
+//                }
+//
+//            }
+//            return null;
+//        }
 
-            }
-            return null;
-        }
 }
