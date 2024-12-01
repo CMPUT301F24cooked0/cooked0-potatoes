dc862060f13f22057f05372069e52f6ad7bc3607
Ociliv <94505855+Ociliv1@users.noreply.github.com>
2024-11-30 21:04:49 -0700

    deleted code for AdministratorBrowseUsersFragmentUnitTest need to add testing

--- src/app/src/test/java/com/example/myapplication/AdministratorBrowseUsersFragmentUnitTest.java
+++ src/app/src/test/java/com/example/myapplication/AdministratorBrowseUsersFragmentUnitTest.java
@@ -21,49 +21,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AdministratorBrowseUsersFragmentUnitTest {
-    private AdministratorBrowseUsersFragment fragment;
-    private ListView listView;
-
-    @Before
-    public void setUp(){
-        Context context = ApplicationProvider.getApplicationContext();
-        fragment=new AdministratorBrowseUsersFragment();
-        ArrayList<User> userList = new ArrayList<>();
-        fragment.userDataList= userList;
-        fragment.userArrayAdapter=new ArrayAdapter<>(context, R.layout.user_profile_summary,fragment.userDataList);
-        listView=new ListView(context);
-        fragment.userList=listView;
-    }
-
-    @Test
-    public void testDeleteSuccess() throws Exception{
-        User testUser=new User("name","name@email.com", 1234567890L);
-        fragment.userDataList.add(testUser);
-        fragment.userArrayAdapter.notifyDataSetChanged();
-        testUser.deleteUser(unused -> {
-            fragment.userDataList.remove(testUser);
-            fragment.userArrayAdapter.notifyDataSetChanged();
-            assertEquals(0, fragment.userDataList.size());
-        },
-                e -> {
-            throw new AssertionError("Failed to delete user");
-        });
-        assertEquals(0,fragment.userDataList.size());
-
-    }
-
-    @Test
-    public void testDeleteFailure() throws Exception{
-        User testUser=new User("name","name@email.com", 1234567890L);
-        fragment.userDataList.add(testUser);
-        fragment.userArrayAdapter.notifyDataSetChanged();
-
-        testUser.deleteUser(unused -> {
-                    throw new AssertionError("Unexpected Deletion");
-                },
-                e -> {
-                    assertEquals(1,fragment.userDataList.size());
-                });
-    }
 
 }
